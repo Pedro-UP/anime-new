517060c5fb5a32375767b71484a4010553e2bdeb
Pedro-UP <pedrocazorla03@gmail.com>
2026-01-07 12:47:20 -0600

    Primer Commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+// 1. Importamos el archivo
+// @ es un alias común para la carpeta 'src', pero también puedes usar './components/Home.vue'
+import Home from './components/Home.vue'
+</script>
+
+<template>
+  <Home />
+</template>
+
